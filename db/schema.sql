7c5673c2a90bec3469332608052281d3c2a88f64
Tacarrem23 <carremitchell729@gmail.com>
2025-07-02 19:04:25 -0400

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,22 @@
--- TODO
+-- db/schema.sql
+CREATE DATABASE jukebox;
+
+\c jukebox;
+
+CREATE TABLE tracks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    artist VARCHAR(255)
+);
+
+CREATE TABLE playlists (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE playlists_tracks (
+    playlist_id INTEGER REFERENCES playlists(id) ON DELETE CASCADE,
+    track_id INTEGER REFERENCES tracks(id) ON DELETE CASCADE,
+    PRIMARY KEY (playlist_id, track_id),
+    UNIQUE (playlist_id, track_id)
+);
